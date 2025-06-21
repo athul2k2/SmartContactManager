ab5d074d1a7fd02ddef61b5edfe252f56fe18290
AthulKrishna <athulkrishnapv6@gmail.com>
2025-06-21 23:57:18 +0530

    testing signup form done

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -3,7 +3,10 @@ package com.smart.controller;
 import com.smart.entities.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -27,6 +30,16 @@ public class HomeController {
         return "signup";
     }
 
+    @RequestMapping(value = "/do_register" , method = RequestMethod.POST)
+    public String registerUser(@ModelAttribute("user") User user,
+                               @RequestParam(value = "agreement",defaultValue = "false")
+                               Boolean agreement,Model model){
+        System.out.println("User:=>"+ user);
+        System.out.println("Agreement;=>"+ agreement);
+
+
+        return "signup";
+    }
 
 
 
