68bc001859c07f63a9ceb5451b69f7c20f9fb80d
AthulKrishna <athulkrishnapv6@gmail.com>
2025-06-21 20:12:27 +0530

    base template and thymleaf setups

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -1,26 +1,26 @@
 package com.smart.controller;
 
-import com.smart.dao.UserRepository;
-import com.smart.entities.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @Autowired
-    private UserRepository userRepo;
-
-    @GetMapping("/test")
-    @ResponseBody
-    public String test(){
-        User user = new User();
-        user.setName("athul");
-        user.setEmail("athul@gmail.com");
-        userRepo.save(user);
-        return "Working";
+    @RequestMapping("/home")
+    public String home(Model model){
+        model.addAttribute("title","Home - SmartContactManager");
+        return "home";
     }
+
+    @RequestMapping("/about")
+    public String about(Model model){
+        model.addAttribute("title","About - SmartContactManager");
+        return "about";
+    }
+
+
+
+
 
 }
