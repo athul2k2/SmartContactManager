3287e9f873a800c6e80a1fc89c224989dd03eb15
AthulKrishna <athulkrishnapv6@gmail.com>
2025-06-25 00:44:37 +0530

    CustomUserDetails and UserDetailServiceImpl added

---
+++ src/main/java/com/smart/config/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.smart.config;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.getUserByUserName(username);
+
+        if(user == null){
+            throw new UsernameNotFoundException("Could not find user !!");
+        }
+
+        return new CustomUserDetails(user);
+    }
+}
