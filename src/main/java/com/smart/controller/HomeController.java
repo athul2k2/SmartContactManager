14e4be3e4f74976aad09811190e736d040cf5d1d
AthulKrishna <athulkrishnapv6@gmail.com>
2025-06-26 00:02:46 +0530

    Minor changes in controller and base (login)

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -9,10 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.User;
@@ -50,6 +47,12 @@ public class HomeController {
             model.addAttribute("user", new User());
         }
         return "signup";
+    }
+
+    @GetMapping("/signin")
+    public String customLogin(Model model){
+        model.addAttribute("title", "Login - SmartContactManager");
+        return "login";
     }
 
     @RequestMapping(value = "/do_register", method = RequestMethod.POST)
@@ -136,5 +139,6 @@ public class HomeController {
             return "signup";
         }
     }
+
 
 }
